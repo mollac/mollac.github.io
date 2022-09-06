9a50d01912e654e3f70d697144672b45f2e01ebc
mollac <molnar.laci@outlook.com>
2022-09-06 11:02:09 +0200

    'office_desktop'

--- js/script.js
+++ js/script.js
@@ -13,11 +13,47 @@ async function getWeatherByLocation(city) {
 
 function addWeatherToPage(data, where) {
     const temp = data.main.temp;
-    const icon = data.weather[0].icon+".png"
+    const icon = data.weather[0].icon + ".png"
     where.innerHTML += `
         <img src="https://openweathermap.org/img/w/${icon}"/>
         <span>${Math.floor(temp)}°C</span>   
         `;
+}
+
+function checkInTime() {
+    const becsengetes = [[8, 0],
+    [8, 55],
+    [9, 50],
+    [10, 50],
+    [11, 45],
+    [12, 40],
+    [13, 35],
+    [14, 30]
+    ]
+    const kicsengetes = [[8, 45],
+    [9, 40],
+    [10, 35],
+    [11, 35],
+    [12, 30],
+    [13, 25],
+    [14, 20],
+    [15, 10]
+    ]
+
+    var currentTime = new Date()
+    var startTime = new Date()
+    var endTime = new Date()
+    for (var i = 0; i < kicsengetes.length; i++) {
+        startTime.setHours(becsengetes[i][0])
+        startTime.setMinutes(becsengetes[i][1])
+        endTime.setHours(kicsengetes[i][0])
+        endTime.setMinutes(kicsengetes[i][1])
+        if ((currentTime.getTime() > startTime.getTime()) && (currentTime.getTime() < endTime.getTime())) {
+            console.log('ÓRA van')
+            return true;
+        }
+    }
+    return false;
 }
 
 
